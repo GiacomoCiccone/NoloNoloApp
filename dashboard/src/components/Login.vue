192dc23575bc496ab1a0755557e72af9455ec624
Matteo Baldazzi <mbaldazzi00@gmail.com>
2022-01-28 17:28:47 +0100

    completamento login autenticato e get per grafici

--- dashboard/src/components/Login.vue
+++ dashboard/src/components/Login.vue
@@ -3,28 +3,28 @@
     <div class="bg-image">
         <div class="inner-block" >      
             <div class="vue-tempalte">
-                <form>
+                <form @submit.prevent="onSubmit">
                     <h3>Sign In</h3>
 
                     <div class="form-group">
                         <label for="email">Email address</label>
-                        <input type="email" name="email" class="form-control form-control-lg" />
+                        <input type="email" name="email" v-model="email" class="form-control form-control-lg" />
                     </div>
 
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control form-control-lg" />
+                        <input name="password" v-model="password" class="form-control form-control-lg" />
                     </div>
                     <br>
                     <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
 
                     <p class="forgot-password text-right mt-2 mb-4">
-                        <router-link to="/forgot-password">Forgot password ?</router-link>
+                        <!-- <a href="/forgotPassword">Forgot password ?<a> -->
                     </p>
 
                 </form>
 
-                <p v-if="showError" id="error">Username or Password is incorrect</p>
+                <p v-if="error">{{error}}</p>
             </div>
         </div>
     </div>    
@@ -32,7 +32,53 @@
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        //name: 'Login'
+        name: 'Login',
+        data () {
+            return {
+                email: '',
+                password: '',
+                error: null,
+            }
+        },
+
+        beforeMount() {
+            if (window.localStorage.getItem("authTokenManager")) this.$router.push({path: '/dashboard'});
+        },
+        methods: {
+        async onSubmit() {
+
+        const body = {
+                email: this.email,
+                password: this.password
+            }
+
+        try {
+
+            const {data} = await axios.put('http://localhost:8000/api/auth/loginManager', body);
+            window.localStorage.setItem("authTokenManager", data.data.authToken);
+            window.localStorage.setItem("userInfoManager", data.data.userInfo);
+
+            this.$router.push({path: '/dashboard'});
+
+        } catch(error) {
+            try {
+                const {data} = await axios.post('http://localhost:8000/api/auth/registerManager', body);
+
+                window.localStorage.setItem("authTokenManager", data.data.authToken);
+                window.localStorage.setItem("userInfoManager", data.data.userInfo);
+
+                this.$router.push({path: '/dashboard'});
+
+            } catch (error) {
+                this.error = error.response.data.error
+                setTimeout(() => {
+                    this.error = null;
+                }, 5000)
+            }
+        }
+    }
+        }
     }
 </script>
